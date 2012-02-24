9c9e142b8d33db6df2dd54dc03fa4f371bb077c4
Paul Oprea <paul.oprea@threepillarglobal.com>
2012-02-24 10:46:24 +0200

    Main project structure in place

---
+++ epoch/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'epoch.views.home', name='home'),
+    # url(r'^epoch/', include('epoch.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns = urlpatterns + patterns('',
+    url(r'^my_uua_consumer/', include('pbs_uua_consumer.urls')),
+)
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                        'README.rst')
+                        
+dependencies = [
+    'django==1.3',
+    'pbs_uua_consumer',
+    'python-openid',
+    'righteous'
+]
+
+dependency_links = [
+    'http://pbs.sourcerepository.s3.amazonaws.com/pbs_uua_consumer-1.0.tar.gz#egg=pbs_uua_consumer-1.0',
+    'http://github.com/Atr3ides/righteous/tarball/master#egg=righteous',
+]
+
+setup(
+    name='epoch',
+    version='0.1',
+    description='PBS App Days prototype of managing Rightscale deployments',
+    long_description=open(README_PATH, 'r').read(),
+    author='App Days Epoch Team',
+    author_email='tpg-pbs-interactive@threepillarglobal.com',
+    url='https://github.com/pbs/epoch',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=dependencies,
+    setup_requires=['versiontools',],
+    dependency_links=dependency_links,
+)
